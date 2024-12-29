d7aad690e1ee91cb00e40c91df374f82dd4b506d
Kunnawuth Meebang <folknawat64@gmail.com>
2024-12-29 21:50:21 +0700

    add pylangnoi

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+setuptools.setup(
+    name="pylangnoi",
+    author="langnoi dev team",
+    author_email="devteam@langnoi.com",
+    description="Langnoi Python Pacakge",
+    license="MIT License",
+    version="0.1.8",
+    packages=setuptools.find_packages(),
+    package_data={"pylangnoi.pyarmor_runtime_000000.windows_x86_64": "pyarmor_runtime.pyd"},
+    install_requires=[
+        "langchain-community",
+        "langchainhub",
+        "langgraph",
+        "mysql-connector-python",
+        "langchain_openai",
+        "langchain-groq",
+        "openpyxl",
+        "python-dotenv",
+        "pymysql",
+        "cryptography",
+        "oauth2client",
+        "gspread",
+    ],
+)
